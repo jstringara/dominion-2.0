5d1f8c846584c5a7d0fe2e2b858a46f64508389e
Jacopo Stringara <62561822+jstringara@users.noreply.github.com>
2021-12-02 17:43:41 +0100

    Registrate le tavole all'admin

--- mysite/main/admin.py
+++ mysite/main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Metadata, Game, Constant
 
 # Register your models here.
+admin.site.register(Metadata)
+admin.site.register(Game)
+admin.site.register(Constant)
