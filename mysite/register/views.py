8fc7fb231c224c137dacf0d1a1bcde6d15cbc1fc
jstringara <jstringara@gmail.com>
2021-12-04 18:35:11 +0100

    Aggiunta app per la gestione degli account

---
+++ mysite/register/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+
+# Create your views here.
+def register(request):
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:   #se get
+        form = UserCreationForm()
+    return render(request, 'register/register.html', {'form': form})
