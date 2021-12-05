ba0cea17653a93729f6a1632019e83b757f4abf4
Jacopo Stringara <62561822+jstringara@users.noreply.github.com>
2021-12-05 06:04:21 +0100

    Aggiunto cambio del nome utente

--- mysite/register/urls.py
+++ mysite/register/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('register', views.register, name='register'),
     path('profile', views.profile, name='profile'),
+    path('username_change', views.username_change, name='username_change')
 ]
