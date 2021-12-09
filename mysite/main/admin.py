85738b56383ed905bd3754fbdd9e6206df627cbc
jstringara <jstringara@gmail.com>
2021-12-09 16:56:08 +0100

    Aggiunti Elo all'admin

--- mysite/main/admin.py
+++ mysite/main/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Metadata, Game, Constant
+from .models import Metadata, Game, Constant, Elo
 
 # Register your models here.
 admin.site.register(Metadata)
 admin.site.register(Game)
 admin.site.register(Constant)
+admin.site.register(Elo)
