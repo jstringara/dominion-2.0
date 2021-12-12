f6286593bb6ed3323448701e70c09638d436a4b2
jstringara <jstringara@gmail.com>
2021-12-12 17:55:06 +0100

    Aggiunta view dell'ultimo torneo

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     path('albo', views.album, name='album'),
     path('nuovo_albo', views.new_album, name='new_album'),
     path('punteggi_attesi', views.get_expected, name='get_expected'),
+    path('ultimo torneo', views.last_tournament, name='last_tournament'),
 ]
