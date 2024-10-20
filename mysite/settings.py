7069b5650afe7f27f28106805757e3305414b0e0
teobucci <teobucci8@gmail.com>
2024-10-20 17:18:07 +0200

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "crispy_forms",
+    "crispy_bootstrap4",
     "register.apps.RegisterConfig",
     "main.apps.MainConfig",
     "sri",
